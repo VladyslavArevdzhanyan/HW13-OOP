6984df50ffb3dbc46e5b89710c60f3da9b4eda7a
Vladyslav <frontendvlados@gmail.com>
2021-12-17 01:19:57 +0200

    bug fixes and change code

--- script.js
+++ script.js
@@ -16,20 +16,23 @@ class TodoList {
     addTodo(todo) {
       this.todos.push(todo);
     }
+
     removeTodo(id) {
       this.todos = this.todos.filter((el) => {
         return el.id !== id;
       });
-      let ulDelete = document.querySelectorAll(`[data-id="${id}"]`)[0];
+      let ulDelete = document.querySelector(`[data-id="${id}"]`);
       ulDelete.remove();
     }
+
     getTodos() {
       return this.todos;
     }
+
     changeStatus(id) {
       let index = this.todos.findIndex((el) => el.id === id);
       this.todos[index].status = !this.todos[index].status;
-      let ulChange = document.querySelectorAll(`[data-id="${id}"]`)[0];
+      let ulChange = document.querySelector(`[data-id="${id}"]`);
 
       if(this.todos[index].status) {
         ulChange.classList.remove('yellow')
@@ -38,21 +41,36 @@ class TodoList {
         ulChange.classList.remove('green')
         ulChange.classList.add('yellow')
       }
-
     }
+
     render() {
       let lis = '';
       for (let el of this.todos) {
         if (!el) {
           return;
         }
-        lis += `<li data-id="${el.id}">${el.value}<button class="delete-task">Delete</button><button class="set-status">Change status</button></li>`;
+        let colorToDo = el.status ? "green" : "yellow";
+        lis += `<li data-id="${el.id}" class ="${colorToDo}">${el.value}<button class="delete-task">Delete</button><button class="set-status">Change status</button></li>`;
       }
       this.el.innerHTML = lis;
     }
 
+    getTodo() {
+      return this.todos;
+    }
+
+    setTodo(todos) {
+      this.todos = todos;
+    }
+
+    searchTodo(inputValue) {
+      let todos = this.getTodo();
+      this.todos = this.todos.filter(todos => todos.value.includes(inputValue));
+      this.render();
+      this.setTodo(todos);
+    }
   }
-  
+
   class Task {
     constructor(value, status) {
       this.value = value;
@@ -61,21 +79,22 @@ class TodoList {
     }
   }
 
-
-
 let input = document.getElementById('inputText');
 let list = document.getElementById('list');
-let btn = document.getElementById('applyBtn')
-let createLi = new TodoList(list);
+let createBtn = document.getElementById('applyBtn');
+let searchToDo = document.getElementById('find');
 
-btn.addEventListener('click', function (event) {
-    let target = event.target;
-    if (target) {
+let createLi = new TodoList(list);
+createBtn.addEventListener('click', function () {
+    if (input.value) {
         createLi.addTodo(new Task(input.value, false));
         createLi.render();
         input.value = '';
     } 
 })
 
-
-
+searchToDo.addEventListener('click', function() {
+  if(input.value) {
+    createLi.searchTodo(input.value);
+  }
+})
